ec11415e86f0d967546d69d7b57eb81b652ccc0d
mskian <veerfun23@gmail.com>
2019-10-08 14:51:43 +0530

    Remove CSS

--- extend.php
+++ extend.php
@@ -13,10 +13,6 @@ use Flarum\Frontend\Document;
 use s9e\TextFormatter\Configurator;
 
 return [
-    (new Extend\Frontend('forum'))
-        ->content(function (Document $document) {
-            $document->head[] = '<link rel="stylesheet" type="text/css" href="/assets/extensions/mskian-tasklist/style.css">';
-        }),
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
             $config->BBCodes->addCustom(
